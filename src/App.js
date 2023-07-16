7863cac5f5799d7c37551cecf178f1c3caa92942
egy351 <129901135+egy351@users.noreply.github.com>
2023-07-16 18:04:30 +0200

    style and functionalities implemented

--- src/App.js
+++ src/App.js
@@ -5,16 +5,84 @@ import Confetti from 'react-confetti';
 
 export default function App() {
   const [dice, setDice] = useState(allNewDice());
-  const [tenzies, setTenzies] = useState(false);
+  const [gameState, setGameState] = useState(defaultGameState());
+  const [start, setStart] = useState(false);
+
   const [showNumbers, setShowNumbers] = useState(false);
-  const [counter, setCounter] = useState(0);
+
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  const [bestScore, setBestScore] = useState(
+    JSON.parse(localStorage.getItem('tenziesBestScore')) || {
+      rolls: null,
+      time: 0,
+    }
+  );
+
+  if (seconds > 59) {
+    setSeconds(0);
+    setMinutes(minute => minute + 1);
+  }
+  if (minutes > 59) {
+    setMinutes(0);
+    setHours(hour => hour + 1);
+  }
+  if (hours > 23) {
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+  }
+
+  function defaultGameState() {
+    return { tenzies: false, rolls: 0, time: 0 };
+  }
+
+  useEffect(() => {
+    if (gameState.tenzies) {
+      if (bestScore.rolls === null) {
+        setBestScore(prevBest => {
+          return { ...prevBest, rolls: gameState.rolls, time: gameState.time };
+        });
+      }
+      if (
+        gameState.rolls < bestScore.rolls ||
+        gameState.time < bestScore.time
+      ) {
+        setBestScore(prevBest => {
+          return { ...prevBest, rolls: gameState.rolls, time: gameState.time };
+        });
+      }
+    }
+  }, [gameState, bestScore]);
+
+  useEffect(() => {
+    localStorage.setItem('tenziesBestScore', JSON.stringify(bestScore));
+  }, [bestScore]);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (!start) {
+        return;
+      }
+      if (gameState.tenzies) {
+        return;
+      }
+      setSeconds(second => second + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [start, gameState.tenzies]);
 
   useEffect(() => {
     const allHeld = dice.every(dice => dice.isHeld);
     const firstValue = dice[0].value;
     const allSameValue = dice.every(dice => dice.value === firstValue);
     if (allHeld && allSameValue) {
-      setTenzies(true);
+      setGameState(prev => {
+        return { ...prev, tenzies: true };
+      });
     }
   }, [dice]);
 
@@ -35,15 +103,22 @@ export default function App() {
   }
 
   function rollDice() {
-    if (!tenzies) {
-      setCounter(counter + 1);
+    if (!gameState.tenzies) {
+      !start && setStart(!start);
+      setGameState(prev => {
+        return { ...prev, rolls: prev.rolls + 1 };
+      });
       setDice(oldDice =>
         oldDice.map(dice => {
           return dice.isHeld ? dice : generateNewDice();
         })
       );
     } else {
-      setTenzies(false);
+      setStart(false);
+      setSeconds(0);
+      setMinutes(0);
+      setHours(0);
+      setGameState(defaultGameState());
       setDice(allNewDice());
     }
   }
@@ -53,6 +128,7 @@ export default function App() {
   }
 
   function holdDice(id) {
+    !start && setStart(!start);
     setDice(oldDice =>
       oldDice.map(dice => {
         return dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice;
@@ -72,19 +148,23 @@ export default function App() {
 
   return (
     <main>
-      {tenzies && <Confetti />}
+      {gameState.tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
-        Roll until all dice are the same. <br /> Click each dice to freeze it at
+        Roll until all dice are the same. <br /> Click each die to freeze it at
         its current value between rolls.
       </p>
-      <p> You rolled {counter} times </p>
+      <p>
+        Time {String(hours).padStart(2, '0')}:{String(minutes).padStart(2, '0')}
+        :{String(seconds).padStart(2, '0')}
+      </p>
+      <p>{gameState.rolls} rolls</p>
       <button onClick={changeDiceFace}>
-        Show {showNumbers ? 'Dices' : 'Numbers'}
+        Show {showNumbers ? 'Dice' : 'Numbers'}
       </button>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        {tenzies ? 'New Game' : 'Roll'}
+        {gameState.tenzies ? 'New Game' : 'Roll'}
       </button>
     </main>
   );
--- src/diceStyle.js
+++ src/diceStyle.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 export const DiceFace = styled.span`
   display: block;
-  transform: scale(var(--ggs, 1));
+  transform: scale(var(--ggs, 2.3));
   position: relative;
   box-sizing: border-box;
   width: 22px;
   height: 22px;
-  border: 2px solid;
+  border: 2px solid transparent;
   border-radius: 3px;
 
   &::before {
