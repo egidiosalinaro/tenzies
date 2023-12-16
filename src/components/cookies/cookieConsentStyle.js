c3184bb8a6c8f26e5adb6c97122c3c954c935eef
Egidio Salinaro <129901135+egidiosalinaro@users.noreply.github.com>
2023-12-16 17:56:48 +0100

    Create cookieConsentStyle.js

---
+++ src/components/cookies/cookieConsentStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CookiesModal = styled.span`
+  display: flex;
+  padding: 2px 10px;
+  max-width: 400px;
+  font-size: 10px;
+  background: white;
+
+  button {
+    margin: 9px 12px;
+  }
+`;
